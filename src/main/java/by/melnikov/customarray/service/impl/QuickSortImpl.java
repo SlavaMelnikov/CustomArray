68aeb2c436cbf97abe56fb58f35c9821eeaddc59
Slava Melnikov <simplejkee@gmail.com>
2023-11-12 20:00:33 +0300

    add quicksort and tests

--- src/main/java/by/melnikov/customarray/service/impl/QuickSortImpl.java
+++ src/main/java/by/melnikov/customarray/service/impl/QuickSortImpl.java
@@ -6,6 +6,37 @@ import by.melnikov.customarray.service.SortArrayService;
 public class QuickSortImpl implements SortArrayService {
     @Override
     public int[] sort(CustomArray array) {
-        return new int[0];
+        int[] customArray = array.getArray();
+        return quickSort(customArray, 0, customArray.length - 1);
+    }
+
+    private int[] quickSort(int[] array, int left, int right) {
+        if (left < right) {
+            int pivot = partition(array, left, right);
+            quickSort(array, left, pivot - 1);
+            quickSort(array, pivot + 1, right);
+        }
+        return array;
+    }
+
+    private int partition(int[] array, int left, int right) {
+        int pivot = right;
+        while (left < pivot) {
+            if (array[left] > array[pivot]) {
+                int temp  = array[pivot - 1];
+                array[pivot - 1] = array[pivot];
+                array[pivot] = temp;
+
+                if (left + 1 != pivot) {
+                    temp = array[left];
+                    array[left] = array[pivot];
+                    array[pivot] = temp;
+                }
+                pivot--;
+                left--;
+            }
+            left++;
+        }
+        return pivot;
     }
 }
